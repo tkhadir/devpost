92e110b31b706028c22eb687a8d52f78ad797efa
KHADIR Tayeb <45130488+tkhadir@users.noreply.github.com>
2020-04-29 13:58:24 +0200

    Update train.py

--- train.py
+++ train.py
@@ -8,49 +8,34 @@ import numpy as np
 
 model = LinearRegression()
 
-
 covid_directory = "~\\COVID-19 Radiography Database\\COVID-19\\"
 normal_directory = "~\\COVID-19 Radiography Database\\NORMAL\\"
-patients_directory= "~\\COVID-19 Radiography Database\\patients\\"
+patients_directory = "~\\COVID-19 Radiography Database\\patients\\"
 
-def train(x, y):
-    model.fit(x, y)
-
-def score(x, y):
-    return model.score(x, y)
-
-def predict(x):
-    model.predict(x)
 
 def rank(filename):
     radio = mpimg.imread(filename)
-    aradio = np.array(radio)
     plt.figure()
     plt.imshow(radio, cmap='gray')
     plt.show()
-    X=aradio.ravel()
-    length = len(X)
-    X_train = X.reshape(length, 1)
-    predictions = predict(X_train)
-    print("predictions == " + str(predictions))
+    X_train = radio.ravel()
+    print("length ==" + str(len(X_train)))
+    pred = model.predict([X_train])
+    print("predictions == " + str(pred))
 
-def analyze(filename, plt):
-    radio = np.array(mpimg.imread(filename))
-    X=radio.ravel()
-    length = len(X)
-    y=np.full((length), 1)
-    X_train = X.reshape(length, 1)
-    train(X_train, y)
-    #print("score == " + str(score(X_train, y)))
-    plt.scatter(X, y)
-    
 
-def getRadios(directory):
-    plt.figure()
+def train(filename, coef):
+    radio = mpimg.imread(filename)
+    X_train = radio.ravel()
+    model.fit([X_train], [coef])
+
+
+def trainRadios(directory, coef):
+    radios = []
     for filename in os.listdir(directory):
-        analyze(directory + filename, plt)
+        train(directory + filename, coef)
         continue
-    plt.show()
+
 
 def rankPatients(directory):
     for filename in os.listdir(directory):
@@ -58,12 +43,14 @@ def rankPatients(directory):
         continue
 
 
-print("training stage")        
-getRadios(covid_directory)
+print("training stage")
+trainRadios(covid_directory, 1)
 print("end training stage")
-print("training stage")        
-getRadios(normal_directory)
+
+print("training stage")
+trainRadios(normal_directory, 0)
 print("end training stage")
+
 
 print("predicting stage")
 rankPatients(patients_directory)
